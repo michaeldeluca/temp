396fa3b0cc3f6e918697fc7bb4ce7829e00989d2
delucamichael <msgmtd@gmail.com>
2019-05-02 22:53:35 +0100

    initial commit

---
+++ KataAPI/Controllers/AliveController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace KataAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AliveController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Connection Successful!");
+        }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EchoNumberController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string number)
+        {
+            if (number == null || !int.TryParse(number, out int item))
+            {
+                return Ok("Please ensure to enter a valid integer!");
+            }
+
+            return Ok($"{number}");
+        }
+    }
+
+   
+}
---
+++ KataAPI/Controllers/CalculationController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KataAPI.Controllers
+{
+    public class Calculation
+    {
+        public int? Answer { get; }
+
+        public Calculation(string valueOne, string valueTwo, string op)
+        {
+            if (
+                (valueOne == null || valueTwo == null) ||
+                (!int.TryParse(valueOne, out var itemOne) ||
+                 !int.TryParse(valueTwo, out var itemTwo))
+            )
+                Answer = null;
+            
+            else Answer = Calculate(Convert.ToInt32(valueOne), op, Convert.ToInt32(valueTwo));
+        }
+
+        private int? Calculate(int numOne, string op, int numTwo)
+        {
+            switch (op)
+            {
+                case "+":
+                    return numOne + numTwo;
+                case "-":
+                    return numOne - numTwo;
+                case "/":
+                    return numOne / numTwo;
+                case "*":
+                    return numOne * numTwo;
+                default: return null;
+            }
+        }
+
+    }
+
+    [Route("api/[controller]/addition")]
+    [ApiController]
+    public class CalculationController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string valueOne, string valueTwo)
+        {
+
+            var calculator = new Calculation(valueOne, "+", valueTwo);
+
+            if (calculator.Answer==null)
+
+                return BadRequest("Please ensure to enter a valid integer!");
+            
+            return Ok($"{calculator.Answer}");
+        }
+    }
+}
